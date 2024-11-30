2b4193aa153fe06ce3f9d70c42b18c7f25730d84
Youssef <kingyoussef76@gmail.com>
2024-11-30 19:04:01 +0200

    add state column to products

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\ProductResource;
-use App\Http\Resources\StoreResource;
-use App\Models\Product;
 use App\Models\Store;
-use App\Traits\ResponseTrait;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Traits\ResponseTrait;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\OrderResource;
+use App\Http\Resources\StoreResource;
+use App\Http\Resources\ProductResource;
 
 class StoreController extends Controller
 {
@@ -117,6 +118,11 @@ class StoreController extends Controller
             $query->where('product_category_id', $request->product_category_id);
         }
 
+        if ($request->has('most_rated') && $request->most_rated) {
+            $query->withCount('ratings')
+                  ->orderBy('ratings_count', 'desc');
+        }
+
         $products = $query->get();
 
         return $this->success(ProductResource::collection($products));
@@ -157,8 +163,61 @@ class StoreController extends Controller
             'country' => $request->country,
             'postal_code' => $request->postal_code,
         ]);
-
         return $this->success(message: 'تمت إضافة الموقع بنجاح');
     }
 
+    public function getUserStoreOrders(Request $request)
+    {
+        $user = Auth::user();
+
+        if (!$user->store) {
+            return $this->error('ليس لديك متجر', 404);
+        }
+
+        $query = $user->store->orders()->with('products');
+
+        if ($request->has('status')) {
+            $query->where('status', $request->status);
+        }
+
+        if ($request->has('date_from') && $request->has('date_to')) {
+            $query->whereBetween('created_at', [$request->date_from, $request->date_to])->orderBy('created_at', 'desc');
+        }
+
+        if ($request->has('user_name')) {
+            $query->whereHas('user', function ($q) use ($request) {
+                $q->where('name', 'like', '%' . $request->user_name . '%');
+            });
+        }
+
+        if ($request->has('user_email')) {
+            $query->whereHas('user', function ($q) use ($request) {
+                $q->where('email', 'like', '%' . $request->user_email . '%');
+            });
+        }
+
+        if ($request->has('user_id')) {
+            $query->where('user_id', $request->user_id);
+        }
+
+        $orders = $query->get()->sortByDesc('created_at');
+        $ordersCount = $orders->count();
+        $totalRevenue = $orders->where('status', 'delivered')->sum('total');
+        $sales = $orders
+            ->where('status', 'delivered')
+            ->flatMap(function ($order) {
+                return $order->products->pluck('pivot.quantity');
+            })
+            ->sum();
+        $canceledOrders = $orders->where('status', 'canceled')->count();
+
+        return $this->success([
+            'order_count' => $ordersCount,
+            'canceled_count' => $canceledOrders,
+            'total_revenue' => $totalRevenue,
+            'sales' => $sales,
+            'orders' => OrderResource::collection($orders),
+        ]);
+    }
+    //state products
 }
